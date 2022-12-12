32e891a19393269feced9ce39967fe462554b550
Gianluchin0 <118599894+Gianluchin0@users.noreply.github.com>
2022-12-12 19:17:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int N;
+    cin >> N;
+    float num[N];
+    for(int i=0; i<N; i++){
+        cin >> num[i];
+    }
+    cout << funzione(num, D);
+    return 0;
 }
