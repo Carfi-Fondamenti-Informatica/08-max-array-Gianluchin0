fe73c0e52589c44df6c1f2250087da4b9ade9076
Gianluchin0 <118599894+Gianluchin0@users.noreply.github.com>
2022-12-12 19:20:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(){
     int N;
     cin >> N;
-    float num[N];
+    float num[D];
     for(int i=0; i<N; i++){
         cin >> num[i];
     }
-    cout << funzione(num, D);
+    cout << funzione(num, N);
     return 0;
 }
